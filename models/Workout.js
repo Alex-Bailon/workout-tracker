6760b35347700edb187808d38c50f748fe920f3b
Alex B <abailon949@gmail.com>
2020-06-03 16:05:59 -0500

    Create Workout.js

---
+++ models/Workout.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        require: [true, "A name is required"]
+    },
+    type: {
+        type: String,
+        require: [true, "A tyoe is required"]
+    },
+    weight: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    duration: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    }
+})
+
+const Workout = mongoose.model('Workout', WorkoutSchema)
+
+module.exports = Workout
