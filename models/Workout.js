11727b8411dd72a97d5d701aa6ddad892fab65fe
Alex B <abailon949@gmail.com>
2020-06-04 10:04:29 -0500

    added total duration

--- models/Workout.js
+++ models/Workout.js
@@ -7,6 +7,9 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
+    totalDuration: {
+        type: Number,
+    },
     exercises: [{
     name: {
         type: String,
